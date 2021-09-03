f7cee8804c41563c5ff9e9862624a35193ed2469
balusena <balusena12@gmail.com>
2021-09-04 02:05:00 +0530

    listpy commit

--- list.py
+++ list.py
@@ -1,5 +1,7 @@
 # This program prints the list using reverse,sort,copy!
 
+import os
+
 List1 = [8,9,3,6,1,10]
 List1.reverse()
 print("The reversed list is", List1)
@@ -12,5 +14,5 @@ List3 = []
 List3 = List1.copy()
 print("List3 =", List3)
 
-Indexvalue = List[2:6]
+Indexvalue = List2[2:6]
 print("The index values are",Indexvalue)
